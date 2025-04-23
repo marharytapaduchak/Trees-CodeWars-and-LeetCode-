6e772a3dc6a614515b399b22a308b7322a77f802
Marharyta <paduchak.pn@ucu.edu.ua>
2025-04-23 17:59:58 +0300

    The task "Sort binary tree by levels" is done. The photo of the result is attached.

--- Delete Node in a BST/task2.py
+++ Delete Node in a BST/task2.py
@@ -1,2 +1,19 @@
+from collections import deque
+
 def tree_by_levels(node):
-    return
+    if node is None:
+        return []
+
+    res = []
+    queue = deque([node])
+
+    while queue:
+        current = queue.popleft()
+        res.append(current.value)
+
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+
+    return res
