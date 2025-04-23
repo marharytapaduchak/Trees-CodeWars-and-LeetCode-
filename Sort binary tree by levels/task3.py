3dbd1b71f242fdc2b94a69784d5a246b30c936d3
Marharyta <paduchak.pn@ucu.edu.ua>
2025-04-23 18:08:31 +0300

    The task "Delete Node in a BST" is done. The photo of the result is attached.

--- Sort binary tree by levels/task3.py
+++ Sort binary tree by levels/task3.py
@@ -1,9 +1,10 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution(object):
     def deleteNode(self, root, key):
         """
@@ -11,4 +12,25 @@ class Solution(object):
         :type key: int
         :rtype: Optional[TreeNode]
         """
-        
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+
+            temp = self.find_min(root.right)
+            root.val = temp.val
+            root.right = self.deleteNode(root.right, temp.val)
+        return root
+
+    def find_min(self, node):
+        while node.left:
+            node = node.left
+        return node
